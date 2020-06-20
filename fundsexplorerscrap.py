e5491e52d0ae059922ed039be73a66c5505196a4
amgsnt <amgsnt@gmail.com>
2020-06-20 13:03:42 -0300

    Realizada remoção de caracteres indesejados

--- fundsexplorerscrap.py
+++ fundsexplorerscrap.py
@@ -3,11 +3,18 @@
 import csv
 import requests
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 url = 'https://www.fundsexplorer.com.br/ranking'
 
+# Data Cleansing
+# 'R$' => ''
+# '%' => ''
+# '.0' => ''
+# '.' => ''
+# ',' => '.'
+# 'N/A' => ''
 
 print("Starting...{}".format(datetime.now()))
 
@@ -23,7 +30,7 @@ table_head = table.find('thead')
 rows = table_head.find_all('tr')
 for row in rows:
     cols = row.find_all('th')
-    colsd = [ele.text.strip() for ele in cols]
+    colsd = [ele.get_text(separator=" ").strip() for ele in cols]
     data.append([ele for ele in colsd])
 
 table_body = table.find('tbody')
@@ -31,7 +38,7 @@ table_body = table.find('tbody')
 rows = table_body.find_all('tr')
 for row in rows:
     cols = row.find_all('td')
-    colsd = [ele.text.strip() for ele in cols]
+    colsd = [ele.text.replace('R$','').replace('%','').replace('.0','').replace('.','').replace('N/A','').replace(',','.').strip() for ele in cols]
     data.append([ele for ele in colsd])
 
 file = open("fii.csv", "w")
